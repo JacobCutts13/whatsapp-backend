ddc7f8ace6d1759eda2dd051dd9bf2606bff6e4f
unknown <jacobdcutts@gmail.com>
2022-06-08 14:47:36 +0100

    added socket io server listeners broadcast message

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,57 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-
-const app = express();
-
-/** Parses JSON data in a request automatically */
-app.use(express.json());
-/** To allow 'Cross-Origin Resource Sharing': https://en.wikipedia.org/wiki/Cross-origin_resource_sharing */
-app.use(cors());
+import http from "http";
+import { Server } from "socket.io";
 
 // read in contents of any environment variables in the .env file
 dotenv.config();
 
-// use the environment variable PORT, or 4000 as a fallback
-const PORT_NUMBER = process.env.PORT ?? 4000;
+const app = express();
 
-// API info page
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "../public/index.html");
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
 
-app.listen(PORT_NUMBER, () => {
-  console.log(`Server is listening on port ${PORT_NUMBER}!`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  const id = socket.handshake.query.id;
+  if (id) {
+    socket.join(id); //static id
+  }
+
+  socket.on(
+    "send message",
+    (message: { recipients: string[]; text: string }) => {
+      console.log(message.text, message.recipients);
+      message.recipients.forEach((recipient) => {
+        const newRecipients = message.recipients.filter((r) => r !== recipient);
+        if (typeof id === "string") newRecipients.push(id);
+        socket.broadcast.to(recipient).emit("recieve message", {
+          recipients: newRecipients,
+          sender: id,
+          text: message.text,
+        });
+      });
+    }
+  );
 });
+
+server.listen(5000, () => {
+  console.log("listening on port 5000");
+});
+
+// // use the environment variable PORT, or 4000 as a fallback
+// const PORT_NUMBER = process.env.PORT ?? 4000;
+
+// // API info page
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "../public/index.html");
+// });
+
+// app.listen(PORT_NUMBER, () => {
+//   console.log(`Server is listening on port ${PORT_NUMBER}!`);
+// });
